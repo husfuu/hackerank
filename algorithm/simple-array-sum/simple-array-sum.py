34a67e4eb3ed618465e8a3dda24d0f3397407d98
husfuu <muhammadhusninurfadillah@gmail.com>
2023-04-15 15:52:52 +0700

    add algorithm sec

---
+++ algorithm/simple-array-sum/simple-array-sum.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    # Write your code here
+    res = 0
+    for i in ar:
+        res += i
+        
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
