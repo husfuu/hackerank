c172f8fe547163107e0e5c3c7a188be5ba1b0326
husfuu <muhammadhusninurfadillah@gmail.com>
2023-04-16 13:25:40 +0700

    update

---
+++ algorithm/plus-minus/plus-minus.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func plusMinus(arr []int32) {
+	// Write your code here
+	var positive = 0
+	var negative = 0
+	var netral = 0
+
+	for i := 0; i < len(arr); i++ {
+		if arr[i] > 0 {
+			positive += 1
+		} else if arr[i] < 0 {
+			negative += 1
+		} else {
+			netral += 1
+		}
+	}
+	var n = float32(len(arr))
+	fmt.Println(float32(positive) / n)
+	fmt.Println(float32(negative) / n)
+	fmt.Println(float32(netral) / n)
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var arr []int32
+
+	for i := 0; i < int(n); i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	plusMinus(arr)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
--- java/java-int-to-string/Solution.java
+++ java/java-int-to-string/Solution.java
@@ -1,3 +1,8 @@
-//Write your code here
-// int n;
-String s = String.valueOf(n);
+class Solution {
+    // Write your code here
+    // int n;
+    public static void main(String[] args) {
+        Integer n = 0;
+        String s = String.valueOf(n);
+    }
+}
